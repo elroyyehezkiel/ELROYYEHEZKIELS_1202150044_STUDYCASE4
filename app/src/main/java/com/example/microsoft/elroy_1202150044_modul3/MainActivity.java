6002e89d5b64fcc16033d363ca0793de90c4afb4
elroyyehezkiel <elroyyehezkiel04@gmail.com>
2018-03-18 20:58:34 +0700

    first commit

---
+++ app/src/main/java/com/example/microsoft/elroy_1202150044_modul3/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.microsoft.elroy_1202150044_modul3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
